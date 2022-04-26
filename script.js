77e7627ff8faaeffbecc686c5cd7e842c993599a
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-04-26 14:06:29 -0700

    first version

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// let btn = document.querySelector("#btn") 
+
+// function myalert () {
+//     alert ("how are you")
+// }
+// btn.onclick = myalert 
+
+
+// (PRIMITIVE TYPES)
+// strings
+let phrase = "I am going home"
+let phrase2 = 'I dey come'
+let phrase3 = `${phrase2} I don show`
+console.log(phrase3);
+// numbers
+let  noOfPens = 30
+// booleans
+let rainySeason = false;
+// null
+let empty =  null;
+// undefined
+// bigInt
+let bigNumber = 2e56
+// symbol
+
+// (REFERENCE/OBJECT TYPES)
+// objects
+// arrays
+// maps
+// sets
